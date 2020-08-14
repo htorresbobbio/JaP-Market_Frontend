23fcb44cbcf705c948ee53cc71ed70bf39520182
Hugo Torres <htorresbobbio@outlook.com>
2020-08-14 10:07:05 -0300

    bugfix: called onLoad function after content load

--- js/init.js
+++ js/init.js
@@ -60,4 +60,5 @@ function signOut() {
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
+  onLoad()
 });
