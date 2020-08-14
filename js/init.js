0279505ac607bce933713390e863a076a57abcae
Hugo Torres <htorresbobbio@outlook.com>
2020-08-14 09:32:32 -0300

    Added Google logout redirection

--- js/init.js
+++ js/init.js
@@ -41,6 +41,21 @@ var getJSONData = function (url) {
     });
 }
 
+//Google Auth Logout
+function onLoad() {
+  gapi.load('auth2', function () {
+    gapi.auth2.init();
+  });
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+    window.location.href = "index.html";
+  });
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
