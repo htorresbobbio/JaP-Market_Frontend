1689850a02a83c311db06e9ac4e8a9f4d3344913
Hugo Torres <htorresbobbio@outlook.com>
2020-09-11 02:09:32 -0300

    modal login form added

--- js/init.js
+++ js/init.js
@@ -48,6 +48,24 @@ function onLoad() {
   });
 }
 
+function onSignIn(googleUser) {
+  // Useful data for your client-side scripts:
+  var profile = googleUser.getBasicProfile();
+  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+  console.log('Full Name: ' + profile.getName());
+  console.log('Given Name: ' + profile.getGivenName());
+  console.log('Family Name: ' + profile.getFamilyName());
+  console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail());
+
+  // The ID token you need to pass to your backend:
+  var id_token = googleUser.getAuthResponse().id_token;
+  console.log("ID Token: " + id_token);
+
+  // After login redirect:
+  window.location.href = "index.html"
+}
+
 function signOut() {
   var loggedWithGoogle = JSON.parse(sessionStorage.getItem('loggedWithGoogle'))
   if (loggedWithGoogle) {
@@ -75,4 +93,25 @@ document.addEventListener("DOMContentLoaded", function (e) {
     userPanel.style.display = "flex";
     loginPanel.style.display = "none";
   }
+
+  var submitButton = document.getElementById("submitButton")
+  submitButton.addEventListener("click", function (e) {
+    let inputUser = document.getElementById("inputUser");
+    let inputPassword = document.getElementById("inputPassword");
+    let isFilled = true;
+
+    if (inputUser.value.trim() === '') {
+      isFilled = false;
+    }
+
+    if (inputPassword.value.trim() === '') {
+      isFilled = false;
+    }
+
+    if (isFilled) {
+      sessionStorage.clear();
+      sessionStorage.setItem('User-Logged', JSON.stringify({ email: inputUser.value.trim() }));
+      window.location = 'index.html';
+    }
+  })
 });
