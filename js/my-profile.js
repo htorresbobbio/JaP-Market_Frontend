df60d5c28122f1952850f47e38af5bfb72ee866a
Hugo Torres <htorresbobbio@outlook.com>
2020-11-04 07:46:45 -0300

    my-profile: added support for avatar change using localstorage

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,32 +1,15 @@
-const defaultAvatar = '<a href="https://imgbb.com/"><img src="https://i.ibb.co/S6dt6zn/Profile-avatar-placeholder-large.png" alt="Profile-avatar-placeholder-large" border="0"></a>'
-
 const profileForm = {
     id: document.querySelector("#profile-form"),
     name: document.querySelector('#name'),
     surname: document.querySelector('#surname'),
-    age: (document.querySelector('#age')),
+    age: document.querySelector('#age'),
     email: document.querySelector('#email'),
     phone: document.querySelector('#phone'),
-    avatar: document.querySelector('#avatar')
+    avatarImage: document.querySelector('#avatar-image'),
+    avatarInput: document.querySelector('#avatar-input'),
 }
 
-function setProfileData() {
-    profileForm.id.addEventListener('submit', (e) => {
-        e.preventDefault()
-        e.stopPropagation()
-        const profileData = {
-            name: profileForm.name.value,
-            surname: profileForm.surname.value,
-            age: parseInt(profileForm.age.value),
-            email: profileForm.email.value,
-            phone: profileForm.phone.value,
-            avatar: profileForm.avatar
-        }
-        localStorage.setItem('profileData', JSON.stringify(profileData))
-    })
-}
-
-function getProfileData() {
+document.addEventListener("DOMContentLoaded", () => {
     profileData = JSON.parse(localStorage.getItem('profileData'))
     if (profileData) {
         profileForm.name.value = profileData.name
@@ -34,16 +17,31 @@ function getProfileData() {
         profileForm.age.value = profileData.age
         profileForm.email.value = profileData.email
         profileForm.phone.value = profileData.phone
-        profileForm.avatar.src = profileData.avatar
+        profileForm.avatarImage.src = profileData.avatar
     } else {
-        profileForm.avatar.src = "https://i.ibb.co/S6dt6zn/Profile-avatar-placeholder-large.png"
+        profileForm.avatarImage.src = "https://i.ibb.co/S6dt6zn/Profile-avatar-placeholder-large.png"
     }
-}
-
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-    getProfileData()
-    setProfileData()
 });
+
+profileForm.avatarInput.addEventListener('change', function () {
+    const reader = new FileReader()
+    reader.addEventListener('load', () => {
+        console.log(reader.result)
+        profileForm.avatarImage.src = reader.result
+    })
+    reader.readAsDataURL(this.files[0])
+})
+
+profileForm.id.addEventListener('submit', (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    const profileData = {
+        name: profileForm.name.value,
+        surname: profileForm.surname.value,
+        age: parseInt(profileForm.age.value),
+        email: profileForm.email.value,
+        phone: profileForm.phone.value,
+        avatar: profileForm.avatarImage.src
+    }
+    localStorage.setItem('profileData', JSON.stringify(profileData))
+})
